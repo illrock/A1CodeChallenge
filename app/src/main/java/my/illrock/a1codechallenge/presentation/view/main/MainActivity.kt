370a1db3c2e8a306b3d21fb7beb1097db605d7b7
illrock <illroman@yandex.ru>
2022-04-21 19:05:57 +0300

    Add splash screen

--- app/src/main/java/my/illrock/a1codechallenge/presentation/view/main/MainActivity.kt
+++ app/src/main/java/my/illrock/a1codechallenge/presentation/view/main/MainActivity.kt
@@ -2,12 +2,14 @@ package my.illrock.a1codechallenge.presentation.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import my.illrock.a1codechallenge.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
